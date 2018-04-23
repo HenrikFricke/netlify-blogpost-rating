9b78e4f3c76b951e7014ca021ec9410bacf3d87f
Henrik Fricke <henrik.fricke@jimdo.com>
2018-04-23 22:19:43 +0200

    Request submission API in function

--- main.go
+++ main.go
@@ -1,14 +1,59 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+type SubmissionData struct {
+	Path string
+	IP   string
+}
+
+type Submission struct {
+	Number int
+	Data   SubmissionData
+}
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+	key := cc.Env["API_KEY"]
+	path := request.QueryStringParameters["path"]
+
+	req, _ := http.NewRequest("GET", "https://api.netlify.com/api/v1/forms/5ade3140e4708575eb7932d4/submissions", nil)
+	req.Header.Set("Authorization", "Authorization: Bearer "+key)
+
+	client := http.Client{}
+	resp, _ := client.Do(req)
+
+	submissions := []Submission{}
+
+	_ = json.NewDecoder(resp.Body).Decode(submissions)
+
+	numberOfLikes := 0
+	for _, submission := range submissions {
+		if submission.Data.Path == path {
+			numberOfLikes = numberOfLikes + 1
+		}
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World",
+		Body:       strconv.Itoa(numberOfLikes),
 	}, nil
 }
 
