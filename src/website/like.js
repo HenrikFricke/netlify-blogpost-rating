7f8414f76d238b2ddca112e81b29c8f589f03007
Henrik Fricke <henrik.fricke@jimdo.com>
2018-04-23 21:17:07 +0200

    Refactor like button to netlify form

--- src/website/like.js
+++ src/website/like.js
@@ -1,10 +1,6 @@
 const likeContainer = document.querySelector("#likecontainer");
 
-const button = document.createElement("button");
-button.innerHTML = "I like";
-
 const numberOfLikes = document.createElement("span");
 numberOfLikes.innerHTML = "0 likes";
 
 likeContainer.appendChild(numberOfLikes);
-likeContainer.appendChild(button);
