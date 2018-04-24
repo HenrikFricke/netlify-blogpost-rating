c98042f2b44aeadb78bd884ea3c7b77e7b943517
Henrik Fricke <henrik.fricke@jimdo.com>
2018-04-24 22:01:44 +0200

    Refactor lambda function to use request

--- functions/generate-likes-file.js
+++ functions/generate-likes-file.js
@@ -1,14 +1,15 @@
-const fetch = require("node-fetch");
+const request = require("request");
 
-exports.handler = async function(event, context, callback) {
-  await fetch(
-    "https://api.netlify.com/build_hooks/" + process.env.BUILD_HOOK_KEY,
+exports.handler = function(event, context, callback) {
+  request.post(
     {
-      method: "POST"
+      url: "https://api.netlify.com/build_hooks/" + process.env.BUILD_HOOK_KEY
+    },
+    function() {
+      return callback(null, {
+        statusCode: 200,
+        body: "Done."
+      });
     }
   );
-
-  callback(null, {
-    statusCode: 200
-  });
 };
