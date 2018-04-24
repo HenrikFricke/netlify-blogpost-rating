bbc0b71823cecc0c62b8c6e7bee818fc9d8fe4ba
Henrik Fricke <henrik.fricke@jimdo.com>
2018-04-24 19:47:17 +0200

    Make form submit from client side

--- src/like.js
+++ src/like.js
@@ -1,12 +1,38 @@
 const likeContainer = document.querySelector("#likecontainer");
-const numberOfLikes = document.createElement("span");
+const likeform = document.querySelector("#likeform");
 
+const numberOfLikes = document.createElement("span");
 likeContainer.appendChild(numberOfLikes);
 
 fetch(".netlify/functions/likes?path=" + window.location.pathname)
   .then(function(res) {
+    if (!res.ok) {
+      return "";
+    }
     return res.text();
   })
   .then(function(likes) {
     numberOfLikes.innerHTML = likes + " likes";
   });
+
+likeform.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const inputElements = event.target.elements;
+  const action = event.target.action;
+
+  var body = new FormData();
+  for (let i = 0; i < inputElements.length; i++) {
+    const element = inputElements[i];
+    if (element.name && element.value) {
+      body.append(element.name, element.value);
+    }
+  }
+
+  fetch(action, {
+    method: "POST",
+    body: body
+  }).then(() => {
+    numberOfLikes.innerHTML = "Thanks for your feedback!";
+  });
+});
